4ef363e82ffe21bc13b8d953970c86d7545e8935
Lawrence Caulker <locaulker@gmail.com>
2023-09-12 06:20:57 -0400

    end L87: auto-batching concept

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [name, setName] = useState("Peter")
+  const [age, setAge] = useState(24)
+  const [hobby, setHobby] = useState("Reading")
 
-export default UseStateObject;
+  const displayPerson = () => {
+    setName("John")
+    setAge(28)
+    setHobby("Screaming at the computer")
+  }
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>Enjoys: {hobby}</h4>
+      <button type="button" className="btn" onClick={displayPerson}>
+        Show John
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
