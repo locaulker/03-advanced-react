42ea92e76223949ccb59a096990c4e16fd937b93
Lawrence Caulker <locaulker@gmail.com>
2023-09-12 17:06:42 -0400

    using a function to update state

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,25 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from "react"
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+  const [value, setValue] = useState(0)
+
+  const handleClick = () => {
+    setValue(currentState => {
+      const newState = currentState + 1
+      console.log(newState)
+      return newState
+    })
+  }
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        Increase
+      </button>
+    </>
+  )
+  return <h2>useState "gotcha"</h2>
+}
+
+export default UseStateGotcha
