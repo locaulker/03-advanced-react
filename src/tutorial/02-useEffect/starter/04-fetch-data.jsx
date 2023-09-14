d381a540f57463d9e9c1d628b6d8983ab701e726
Lawrence Caulker <locaulker@gmail.com>
2023-09-14 01:30:58 -0400

    end L97

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,6 @@
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users"
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
-};
-export default FetchData;
+  return <h2>fetch data example</h2>
+}
+export default FetchData
