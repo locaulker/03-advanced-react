3327fe35ece41939f8c5d41faf6317432745496d
Lawrence Caulker <locaulker@gmail.com>
2023-09-15 11:18:32 -0400

    fetched user

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,22 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react"
+const url = "https://api.github.com/users/QuincyLarson"
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
-};
-export default MultipleReturnsFetchData;
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchedUser = async () => {
+      try {
+        const resp = await fetch(url)
+        const user = await resp.json()
+        console.log(user)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchedUser()
+  }, [])
+
+  return <h2>Fetch Data</h2>
+}
+export default MultipleReturnsFetchData
