62044d26f2262aba074d6a94775327bc7f0bdf11
Lawrence Caulker <locaulker@gmail.com>
2023-09-25 01:25:24 -0400

    implemented select input

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -3,9 +3,15 @@ const frameworks = ["react", "angular", "vue", "svelte"]
 
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false)
+  const [framework, setFramework] = useState("react")
+
   const handleShipping = (e) => {
     console.log(e.target.checked)
     setShipping(e.target.checked)
+  }
+
+  const handleFramework = (e) => {
+    setFramework(e.target.value)
   }
 
   return (
@@ -27,6 +33,16 @@ const OtherInputs = () => {
           <label htmlFor="framework" className="form-label">
             Framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={framework}
+            onChange={handleFramework}
+          >
+            {frameworks.map((framework) => {
+              return <option key={framework}>{framework}</option>
+            })}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
           submit
