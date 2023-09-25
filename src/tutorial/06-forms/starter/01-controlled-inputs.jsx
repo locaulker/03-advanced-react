680aec7fa8ebbf4ba3d9f07a94250b58a68faf38
Lawrence Caulker <locaulker@gmail.com>
2023-09-24 21:11:08 -0400

    how to setup controlled inputs in react

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,21 +1,51 @@
+import { useState } from "react"
+
 const ControlledInputs = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  const handleChange = (e) => {
+    // console.log(e.target.name)
+    // console.log(e.target.value)
+    setName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // this is where you do something to the values collected in the controlled inputs
+    console.log(name, email)
+  }
+
   return (
-    <form className="form">
-      <h4>controlled inputs</h4>
+    <form className="form" onSubmit={handleSubmit}>
+      <h4>Controlled Inputs</h4>
       <div className="form-row">
         <label htmlFor="name" className="form-label">
           name
         </label>
-        <input type="text" className="form-input" id="name" />
+        <input
+          type="text"
+          className="form-input"
+          id="name"
+          value={name}
+          onChange={handleChange}
+        />
       </div>
       <div className="form-row">
         <label htmlFor="email" className="form-label">
           Email
         </label>
-        <input type="email" className="form-input" id="email" />
+        <input
+          type="email"
+          className="form-input"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <button type="submit" className="btn btn-block">
-        submit
+        Submit
       </button>
     </form>
   )
