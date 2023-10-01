84ac4bf1a2a3aca59feb568b50c17d0d10c0aa85
Lawrence Caulker <locaulker@gmail.com>
2023-09-30 21:32:58 -0400

    intro to context API

--- src/tutorial/09-context-api/starter/Navbar.jsx
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react"
+import { useState, createContext } from "react"
 import NavLinks from "./NavLinks"
+
+export const NavbarContext = createContext()
 
 const Navbar = () => {
   const [user, setUser] = useState({ name: "Bob" })
@@ -9,10 +11,12 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="navbar">
-      <h5>CONTEXT API</h5>
-      <NavLinks user={user} logout={logout} />
-    </nav>
+    <NavbarContext.Provider value={{ user, logout }}>
+      <nav className="navbar">
+        <h5>CONTEXT API</h5>
+        <NavLinks />
+      </nav>
+    </NavbarContext.Provider>
   )
 }
 export default Navbar
