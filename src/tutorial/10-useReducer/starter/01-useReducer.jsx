1040d8caa11d1ab15a96f56449e75d85bb20d2b0
Lawrence Caulker <locaulker@gmail.com>
2023-10-07 04:05:15 -0400

    implemented basic reducer setup using if statements instead of switch syntax

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -2,10 +2,15 @@ import { useState, useReducer } from "react"
 import { data } from "../../../data"
 
 const defaultState = {
-  people: data
+  people: data,
+  isLoading: false
 }
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_LIST") {
+    return { ...state, people: [] }
+  }
+}
 
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState)
@@ -18,6 +23,7 @@ const ReducerBasics = () => {
   }
 
   const clearList = () => {
+    dispatch({ type: "CLEAR_LIST" })
     // setPeople([])
   }
 
