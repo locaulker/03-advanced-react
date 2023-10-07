e427482af26efff9177f668336a42590a28ec575
Lawrence Caulker <locaulker@gmail.com>
2023-10-07 04:23:46 -0400

    setup actions and throw new error

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -6,10 +6,16 @@ const defaultState = {
   isLoading: false
 }
 
+const CLEAR_LIST = "CLEAR_LIST"
+const reset_LIST = "reset_LIST"
+const REMOVE_ITEM = "REMOVE_ITEM"
+
 const reducer = (state, action) => {
   if (action.type === "CLEAR_LIST") {
     return { ...state, people: [] }
   }
+  // return state
+  throw new Error(`No matching "${action.type}" - action type`)
 }
 
 const ReducerBasics = () => {
@@ -23,11 +29,12 @@ const ReducerBasics = () => {
   }
 
   const clearList = () => {
-    dispatch({ type: "CLEAR_LIST" })
+    dispatch({ type: CLEAR_LIST })
     // setPeople([])
   }
 
   const resetList = () => {
+    dispatch({ type: "something" })
     // setPeople(data)
   }
 
